5621198537ccf35eb1f9372033ff60822df55dd1
Brendan Burch <44724846+Brendan525@users.noreply.github.com>
2020-07-30 16:04:01 -0400

    Completed Lab

--- Program.cs
+++ Program.cs
@@ -41,10 +41,32 @@ namespace Lab_5._2
 
     class HumanPlayer : Player
     {
-
-        public override Roshambo GenerateRoshambo(HumanPlayer player)
+        public override Roshambo GenerateRoshambo()
         {
-            HumanPlayer pl
+            Console.WriteLine("Rock, Paper, or Scissors (R/P/S): "); // Question
+
+            while (true)
+            {
+                string rps = Console.ReadLine().ToUpper(); // Asks for input
+
+                if (rps == "R")
+                {
+                    return Roshambo.Rock;
+                }
+                else if (rps == "P")
+                {
+                    return Roshambo.Paper;
+                }
+                else if (rps == "S")
+                {
+                    return Roshambo.Scissors;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter (R/P/S)");
+                    continue;
+                }
+            }
             
         }
     }
@@ -54,42 +76,73 @@ namespace Lab_5._2
     {
         static void Main(string[] args)
         {
-            //string inputname;
-            //string versus;
-            //Console.WriteLine("Welcome to Rock Paper Scissors!");
-
-            //Console.WriteLine("Enter your nam: ");
-
-            //inputname = Console.ReadLine();
-
-            //Console.WriteLine($"Hello {inputname}, Would you like to play against TheJets or TheSharks (j/s)");
-
-            //versus = Console.ReadLine().ToLower();
-
-            //if (versus == "j")
-            //{
-                
-            //}
-            //else if(versus == "s")
-            //{
-
-            //}
-            //else if (versus != "j" | versus != "s")
-            //{
-            //    Console.WriteLine("Invalid option");
-            //}
-
             Rock rockplayer = new Rock();
+            HumanPlayer player = new HumanPlayer();
+            RandomPlayer rand = new RandomPlayer();
+            string inputname, versus;
 
-            Console.WriteLine(rockplayer.GenerateRoshambo()); // Prints Rock
+            Console.WriteLine("Welcome to Rock Paper Scissors!");
 
-            RandomPlayer randomPlayer = new RandomPlayer();
+            Console.WriteLine("Enter your name: ");
 
-            Console.WriteLine(randomPlayer.GenerateRoshambo()); // Randomly prints Rock, Paper, or Scissors
+            inputname = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine($"Would you like to play against TheJets or TheSharks (J/S)");
 
-            
+                versus = Console.ReadLine().ToUpper();
 
+                if (versus == "J")
+                {
+                    Roshambo userinput = player.GenerateRoshambo(); // Saves the users input into another variable
 
+                    Console.WriteLine($"TheJets: {rockplayer.GenerateRoshambo()}"); // Prints Rock
+
+                    if (userinput == Roshambo.Rock)
+                    {
+                        Console.WriteLine("It's a Tie");
+                    }
+                    else if (userinput == Roshambo.Scissors)
+                    {
+                        Console.WriteLine("TheJets win");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{inputname} wins.");
+                    }
+                }
+                else if (versus == "S")
+                {
+                    Roshambo userInput = player.GenerateRoshambo(); // Saves the users input into another variable    
+                    Console.WriteLine($"{inputname}: {userInput}"); // Prints Rock
+
+                    Roshambo randomInput = rand.GenerateRoshambo();
+                    Console.WriteLine($"TheSharks: {randomInput}");
+
+                    WinLoseDraw(ref inputname, userInput, randomInput);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option");
+                    continue;
+                }
+            }
+        }
+
+        public static void WinLoseDraw(ref string inputname, Roshambo userInput, Roshambo randomInput)
+        {
+            if (userInput == randomInput)
+            {
+                Console.WriteLine("It's a Tie");
+            }
+            else if (userInput == Roshambo.Rock && randomInput == Roshambo.Paper || userInput == Roshambo.Paper && randomInput == Roshambo.Scissors || userInput == Roshambo.Scissors && randomInput == Roshambo.Rock)
+            {
+                Console.WriteLine("TheSharks win");
+            }
+            else
+            {
+                Console.WriteLine($"{inputname} wins");
+            }
         }
     }
 }
