b1a7ee6efa1e084709b4b179328926c9a9d95ac5
Brendan Burch <44724846+Brendan525@users.noreply.github.com>
2020-07-31 12:38:01 -0400

    Added name to Player Class

--- Player.cs
+++ Player.cs
@@ -6,6 +6,8 @@ namespace Lab_5._2
 {
     abstract class Player
     {
+        public string Name { get; set; }
+
         public Roshambo theroshambo { get; set; }
 
         public abstract Roshambo GenerateRoshambo();
